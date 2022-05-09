c82d32e6edaaf5b54825768f7fd07b7801d22d6e
Mike Wright <m.wright92@icloud.com>
2022-05-09 15:19:06 -0700

    build volume and cost_to_ship methods
    co-authored-by: Brady Diamond <brdydmnd@gmail.com>

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -1,11 +1,12 @@
 class Parcel
-  attr_reader :length, :width, :weight, :id
+  attr_reader :length, :width, :weight, :height, :id
   @@parcels = {}
   @@total_rows = 0
 
-  def initialize(length, width, weight, id)
+  def initialize(length, width, height, weight, id)
     @length = length
     @width = width
+    @height = height
     @weight = weight
     @id = id || @@total_rows += 1
   end
@@ -18,9 +19,10 @@ class Parcel
     @@parcels.delete(self.id)
   end
 
-  def update(length, width, weight)
+  def update(length, width, weight, height)
     @length = length
     @width = width
+    @height = height
     @weight = weight
   end
 
@@ -29,7 +31,7 @@ class Parcel
   end
 
   def save
-    @@parcels[self.id] = Parcel.new(self.length, self.width, self.weight, self.id)
+    @@parcels[self.id] = Parcel.new(self.length, self.width, self.height, self.weight, self.id)
   end
 
   def self.clear
@@ -39,5 +41,14 @@ class Parcel
 
   def self.find(id)
     @@parcels[id]
+  end
+
+  def volume
+    @volume = (@length * @width * @height)
+  end
+
+  def cost_to_ship
+    volume
+    cost = ((@weight * @volume) / 100)
   end
 end
--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -13,18 +13,18 @@ describe '#parcel' do
   end 
   describe('#save') do
     it("saves a parcel") do
-      parcel = Parcel.new(5, 5, 10, nil)
+      parcel = Parcel.new(5, 5, 10, 5, nil)
       parcel.save()
-      parcel2 = Parcel.new(3, 5, 8, nil)
+      parcel2 = Parcel.new(3, 5, 8, 5, nil)
       parcel2.save()
       expect(Parcel.all).to(eq([parcel,parcel2]))
     end
   end 
   describe('.clear') do
     it("clears all parcels") do
-      parcel = Parcel.new(5, 5, 10, nil)
+      parcel = Parcel.new(5, 5, 10, 5, nil)
       parcel.save()
-      parcel2 = Parcel.new(3, 5, 8, nil)
+      parcel2 = Parcel.new(3, 5, 8, 5, nil)
       parcel2.save()
       Parcel.clear()
       expect(Parcel.all).to(eq([]))
@@ -32,31 +32,44 @@ describe '#parcel' do
   end
   describe('.find') do
     it("finds an parcel by id") do
-      parcel = Parcel.new(5, 5, 10, nil)
+      parcel = Parcel.new(5, 5, 10, 5, nil)
       parcel.save()
-      parcel2 = Parcel.new(3, 5, 8, nil)
+      parcel2 = Parcel.new(3, 5, 8, 5, nil)
       parcel2.save()
       expect(Parcel.find(parcel.id)).to(eq(parcel))
     end
   end
   describe('#update') do
     it("updates an parcel by id") do
-      parcel = Parcel.new(3, 5, 8, nil)
+      parcel = Parcel.new(3, 5, 8, 8, nil)
       parcel.save()
-      parcel.update(7,7,12)
+      parcel.update(7,7,12,12)
       expect(parcel.length).to(eq(7))
       expect(parcel.width).to(eq(7))
       expect(parcel.weight).to(eq(12))
+      expect(parcel.height).to(eq(12))
     end
   end
   describe('#delete') do
     it("deletes a parcel by id") do
-      parcel = Parcel.new(5, 5, 10, nil)
+      parcel = Parcel.new(5, 5, 10, 10, nil)
       parcel.save()
-      parcel2 = Parcel.new(3, 5, 8, nil)
+      parcel2 = Parcel.new(3, 5, 8, 10, nil)
       parcel2.save()
       parcel.delete()
       expect(Parcel.all).to(eq([parcel2]))
+    end
+  end
+  describe('#volume') do
+    it("provides volume of a parcel") do
+      parcel = Parcel.new(5, 5, 10, 10, nil)
+      expect(parcel.volume).to(eq(250))
+    end
+  end
+  describe('#cost_to_ship') do
+    it("provides cost to ship a parcel") do
+      parcel = Parcel.new(5, 5, 10, 10, nil)
+      expect(parcel.cost_to_ship).to(eq(25))
     end
   end
 end
